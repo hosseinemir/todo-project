9ce5522e0697987df875280746fa95b374b05364
hosseinemir <hosseinemir79@gmail.com>
2023-08-27 04:54:32 +0330

    fix bugs

--- pages/[todoId].js
+++ pages/[todoId].js
@@ -2,7 +2,7 @@ import TodoDetails from "@/components/template/TodoDetails";
 import { useRouter } from "next/router";
 import { useEffect, useState } from "react";
 import { getSession } from "next-auth/react";
-export default function todoId() {
+export default function Todoid() {
   const [data, setData] = useState("");
   const router = useRouter();
   const { todoId } = router.query;
